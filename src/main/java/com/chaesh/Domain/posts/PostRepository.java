eb733e217f3aaad672a924c7a8ab289a6842f823
DESKTOP-NOBLUE1\chae <kevin5260334@naver.com>
2023-07-13 13:46:41 +0900

    7/13 1

--- src/main/java/com/chaesh/Domain/posts/PostRepository.java
+++ src/main/java/com/chaesh/Domain/posts/PostRepository.java
@@ -1,6 +1,5 @@
 package com.chaesh.Domain.posts;
 
-import jakarta.ws.rs.POST;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
